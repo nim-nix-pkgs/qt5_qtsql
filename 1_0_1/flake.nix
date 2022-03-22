94de35f539f19f20fe03588c49313f35236c7144
hugosenari <hugosenari@gmail.com>
2022-03-22 01:15:34 -0300

    chore: re-index ../nimpkgs/q/qt5_qtsql

--- 1_0_1/flake.nix
+++ 1_0_1/flake.nix
@@ -8,8 +8,8 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-qt5_qtsql-1_0_1.flake = false;
+  inputs.src-qt5_qtsql-1_0_1.ref   = "refs/tags/1.0.1";
   inputs.src-qt5_qtsql-1_0_1.owner = "philip-wernersbach";
-  inputs.src-qt5_qtsql-1_0_1.ref   = "1_0_1";
   inputs.src-qt5_qtsql-1_0_1.repo  = "nim-qt5_qtsql.git";
   inputs.src-qt5_qtsql-1_0_1.type  = "github";
   
